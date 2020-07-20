6996cbe822713c7a291ac2cc78dc34cca04df642
BestKora <developer@bestkora.com>
2020-07-20 08:07:03 +0100

    lecture 5 added

---
+++ Memorize L5/Memorize/Cardify.swift
@@ -0,0 +1,34 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by CS193P Instructor on 04/06/2020.
+//  Copyright © 2020 cs193p instructor. All rights reserved.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius:cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                    RoundedRectangle(cornerRadius:cornerRadius).fill()
+            }
+        }
+    }
+    
+    private let cornerRadius: CGFloat = 10.0
+    private let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
--- Memorize L5/Memorize/EmojiMemoryGameView.swift
+++ Memorize L5/Memorize/EmojiMemoryGameView.swift
@@ -32,31 +32,29 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     private func body (for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius:cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius:cornerRadius).stroke(lineWidth: edgeLineWidth)
+        if card.isFaceUp || !card.isMatched {
+            ZStack {
+                Pie(startAngle: Angle.degrees(0 - 90),
+                    endAngle: Angle.degrees(110 - 90),
+                    clockwise: true)
+                    .padding(5).opacity(0.4)
                 Text(card.content)
-            } else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius:cornerRadius).fill()
-                }
+                    .font(Font.system(size: fontSize(for: size)))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
-        .font(Font.system(size: fontSize(for: size)))
     }
     
      // MARK:  Drawing Constants
     
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3
-    
     private func fontSize(for size:CGSize) -> CGFloat {
-         min(size.width, size.height) *  0.75
+         min(size.width, size.height) *  0.65
 
     }
 }
+
 
 
 
@@ -73,27 +71,8 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// return RoundedRectangle (cornerRadius: 10.0)
